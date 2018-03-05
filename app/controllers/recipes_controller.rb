e390fe3ce8100843bce5eef4a2f210a2d57a0860
hambrice <rbh0005@gmail.com>
2018-03-05 13:06:12 -0800

    built nested resource for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,9 @@ class RecipesController < ApplicationController
   end
 
   def index
-    if params[:query] != nil
+    if params[:user_id]
+      @recipes = User.find(params[:user_id]).recipes
+  elsif params[:query] != nil
       @recipes = Recipe.all.select {|s| s.name.downcase.include?(params[:query].downcase)}
     else
     #@categories = ["Cake","Frozen", "Cookie", "Pie", "Candy", "Pastry", "Other"]
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   root 'site#index'
   get 'profiles/:id', to: 'site#show', as: 'user'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users do
+    resources :recipes, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
